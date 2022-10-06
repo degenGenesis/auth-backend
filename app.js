67bda69a5c7d98c5150ccf26f2862a6b619ed709
Trevor Jones <degengenesis@protonmail.com>
2022-10-06 09:09:32 -0700

    Install: bcrypt |  Add: /register endpoint and associated logic

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const bcrypt = require('bcrypt');
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+
 const dbConnect = require('./db/dbConnect');
+const User = require('./db/userModel');
 
 // connect to database
 dbConnect();
@@ -15,5 +18,32 @@ app.get("/", (request, response, next) => {
   next();
 });
 
+app.post('/register', (request, response) => {
+  // hash password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create new user
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: 'User created successfully!',
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: 'Password was not hashed correctly',
+            error,
+          });
+        });
+    });
+});
 
 module.exports = app;
